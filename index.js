fc1bc3ab301a18149cf10611b99818b4a1ea2deb
Duy Pham <ericpham19@outlook.com>
2022-07-13 16:18:02 +1000

    Completed lab

---
+++ index.js
@@ -0,0 +1,50 @@
+function add() {}
+
+function subtract() {}
+
+function multiply() {}
+
+function divide() {}
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a , b){
+    return a-b;
+}
+
+function multiply(a , b){
+    return a*b;
+}
+
+function divide(a, b){
+    return a/b
+}
+
+
+
+function increment(a) {
+  
+    return a + 1;
+}
+
+function decrement(a){
+    return a-1;
+}
+let result;
+function makeInt(n){
+    return result=parseInt(n,10);
+}
+
+function preserveDecimal(n){
+    return result=parseFloat(n);
+}
+
+
+
+
+
+
+
+
